9f5b5356c2d04631fe971abba703db4f4e1103d2
Boris Leveikin <35217456+Borisl90@users.noreply.github.com>
2019-03-24 15:58:55 +0200

    test2

--- test/unit/movieFeature.spec.js
+++ test/unit/movieFeature.spec.js
@@ -35,7 +35,15 @@ describe('MovieFeature [unit]', () => {
 
         it('retrieve movie only between specific years', async () => {
             const { rating, fromDate, toDate } = { rating: 5, fromDate: 2015, toDate: 2016 }
-            let data = await getHttpRequest('https://api.themoviedb.org/3/movie/550?api_key=eaa197b250138af7cf36467821b800d1')
+            let data = await getHttpRequest('https://api.themoviedb.org/3/discover/movie?api_key=eaa197b250138af7cf36467821b800d1',
+                {
+                    language: 'en-US',
+                    sort_by: 'vote_average.asc',
+                    include_adult: false,
+                    include_video: false,
+                    page: 1,
+                    with_original_language: 'en'
+                })
             const result = MovieFeature.yearsAndPopluritySearch(data, rating, fromDate, toDate)
             expect(result).to.be.an('array')
             expect(result.every((item) => item.release_date >= fromDate && item.release_date <= toDate)).to.be.eql(true)
@@ -43,7 +51,15 @@ describe('MovieFeature [unit]', () => {
 
         it('contain movie that projected in specifics year', async () => {
             const { rating, fromDate, toDate } = { rating: 5, fromDate: 2015, toDate: 2016 }
-            let data = await getHttpRequest('https://api.themoviedb.org/3/movie/550?api_key=eaa197b250138af7cf36467821b800d1')
+            let data = await getHttpRequest('https://api.themoviedb.org/3/discover/movie?api_key=eaa197b250138af7cf36467821b800d1',
+                {
+                    language: 'en-US',
+                    sort_by: 'vote_average.asc',
+                    include_adult: false,
+                    include_video: false,
+                    page: 1,
+                    with_original_language: 'en'
+                })
             const result = MovieFeature.yearsAndPopluritySearch(data, rating, fromDate, toDate)
             expect(result).to.be.an('array')
             expect(result).to.deep.include({ movieName: 'tom and jery', year: 2012 })
@@ -51,14 +67,30 @@ describe('MovieFeature [unit]', () => {
 
         it('retrieve an empty list for false search result', async () => {
             const { rating, fromDate, toDate } = { rating: 5, fromDate: 2999, toDate: 3016 }
-            let data = await getHttpRequest('https://api.themoviedb.org/3/movie/550?api_key=eaa197b250138af7cf36467821b800d1')
+            let data = await getHttpRequest('https://api.themoviedb.org/3/discover/movie?api_key=eaa197b250138af7cf36467821b800d1',
+                {
+                    language: 'en-US',
+                    sort_by: 'vote_average.asc',
+                    include_adult: false,
+                    include_video: false,
+                    page: 1,
+                    with_original_language: 'en'
+                })
             const result = MovieFeature.yearsAndPopluritySearch(data, rating, fromDate, toDate)
             expect(result).to.be.an('array').and.to.have.lengthOf(0)
         })
 
         it('raise exception when years range is invalid', async () => {
             const { rating, fromDate, toDate } = { rating: 5, fromDate: 2016, toDate: 2015 }
-            let data = await getHttpRequest('https://api.themoviedb.org/3/movie/550?api_key=eaa197b250138af7cf36467821b800d1')
+            let data = await getHttpRequest('https://api.themoviedb.org/3/discover/movie?api_key=eaa197b250138af7cf36467821b800d1',
+                {
+                    language: 'en-US',
+                    sort_by: 'vote_average.asc',
+                    include_adult: false,
+                    include_video: false,
+                    page: 1,
+                    with_original_language: 'en'
+                })
             expect(() => MovieFeature.yearsAndPopluritySearch(data, rating, fromDate, toDate)).to.throw(Error)
         })
     })
@@ -67,7 +99,15 @@ describe('MovieFeature [unit]', () => {
 
         it('retrieve a list with all Earning negative and positive', async () => {
             const { rating, fromDate, toDate } = { rating: 5, fromDate: 2015, toDate: 2016 }
-            let data = await getHttpRequest('https://api.themoviedb.org/3/movie/550?api_key=eaa197b250138af7cf36467821b800d1')
+            let data = await getHttpRequest('https://api.themoviedb.org/3/discover/movie?api_key=eaa197b250138af7cf36467821b800d1',
+                {
+                    language: 'en-US',
+                    sort_by: 'vote_average.asc',
+                    include_adult: false,
+                    include_video: false,
+                    page: 1,
+                    with_original_language: 'en'
+                })
             const result = MovieFeature.yearsAndPopluritySearch(data, rating, fromDate, toDate)
             expect(result).to.be.an('array')
             expect(result.every((item) => item.profit >= 0 || item.profit <= 0)).to.be.eql(true)
@@ -75,7 +115,15 @@ describe('MovieFeature [unit]', () => {
 
         it('filter movies without information about profit', async () => {
             const { rating, fromDate, toDate } = { rating: 5, fromDate: 2015, toDate: 2016 }
-            let data = await getHttpRequest('https://api.themoviedb.org/3/movie/550?api_key=eaa197b250138af7cf36467821b800d1')
+            let data = await getHttpRequest('https://api.themoviedb.org/3/discover/movie?api_key=eaa197b250138af7cf36467821b800d1',
+                {
+                    language: 'en-US',
+                    sort_by: 'vote_average.asc',
+                    include_adult: false,
+                    include_video: false,
+                    page: 1,
+                    with_original_language: 'en'
+                })
             const result = MovieFeature.yearsAndPopluritySearch(data, rating, fromDate, toDate)
             expect(result).to.be.an('array')
             expect(result).to.not.deep.include({ movieName: 'tom and jery' })
@@ -83,14 +131,30 @@ describe('MovieFeature [unit]', () => {
 
         it('retrieve a sorted list', async () => {
             const { rating, fromDate, toDate } = { rating: 5, fromDate: 2015, toDate: 2016 }
-            let data = await getHttpRequest('https://api.themoviedb.org/3/movie/550?api_key=eaa197b250138af7cf36467821b800d1')
+            let data = await getHttpRequest('https://api.themoviedb.org/3/discover/movie?api_key=eaa197b250138af7cf36467821b800d1',
+                {
+                    language: 'en-US',
+                    sort_by: 'vote_average.asc',
+                    include_adult: false,
+                    include_video: false,
+                    page: 1,
+                    with_original_language: 'en'
+                })
             const result = MovieFeature.yearsAndPopluritySearch(data, rating, fromDate, toDate)
             expect(result).to.be.an('array').and.to.be.sortedBy('profit')
         })
 
         it('filter movie that not showed yet', async () => {
             const { rating, fromDate, toDate } = { rating: 5, fromDate: 2015, toDate: 2016 }
-            let data = await getHttpRequest('https://api.themoviedb.org/3/movie/550?api_key=eaa197b250138af7cf36467821b800d1')
+            let data = await getHttpRequest('https://api.themoviedb.org/3/discover/movie?api_key=eaa197b250138af7cf36467821b800d1',
+                {
+                    language: 'en-US',
+                    sort_by: 'vote_average.asc',
+                    include_adult: false,
+                    include_video: false,
+                    page: 1,
+                    with_original_language: 'en'
+                })
             const result = MovieFeature.yearsAndPopluritySearch(data, rating, fromDate, toDate)
             expect(result).to.be.an('array')
             expect(result.every((item) => item.release_date <= Date.now())).to.be.eql(true)
@@ -99,7 +163,15 @@ describe('MovieFeature [unit]', () => {
 
         it('at least 7 days from premiere', async () => {
             const { rating, fromDate, toDate } = { rating: 5, fromDate: 2015, toDate: 2016 }
-            let data = await getHttpRequest('https://api.themoviedb.org/3/movie/550?api_key=eaa197b250138af7cf36467821b800d1')
+            let data = await getHttpRequest('https://api.themoviedb.org/3/discover/movie?api_key=eaa197b250138af7cf36467821b800d1',
+                {
+                    language: 'en-US',
+                    sort_by: 'vote_average.asc',
+                    include_adult: false,
+                    include_video: false,
+                    page: 1,
+                    with_original_language: 'en'
+                })
             const result = MovieFeature.yearsAndPopluritySearch(data, rating, fromDate, toDate)
             expect(result).to.be.an('array')
             expect(result.every((item) => item.release_date <= Date.now().setDate(getDate() - 7))).to.be.eql(true)
